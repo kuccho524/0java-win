53514b83771f476ec3135a5dac4df10ddc3e72f8
kuccho524 <kucchachanocha@gmail.com>
2021-07-22 21:18:19 +0900

    Add hello

---
+++ Hello2.java
@@ -0,0 +1,10 @@
+class Hello2 {
+  public static void say(String s) {
+    System.out.println(s);
+  }
+
+  public static void main(String[] args) {
+    String message = "Hello JAVA!";
+    say(message);
+  }
+}  
